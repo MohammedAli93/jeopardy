3f272b06caef5b113ad0ce057406035f9659f7f3
Andres <costandres8@gmail.com>
2025-07-12 21:31:55 -0400

    misc: improve code readability by formatting and organizing main menu scene

--- src/scenes/main-menu/main-menu.scene.ts
+++ src/scenes/main-menu/main-menu.scene.ts
@@ -10,25 +10,51 @@ export class MainMenuScene extends Phaser.Scene {
     const VIDEO_HEIGHT = 720;
 
     // Background
-    const video = this.add.video(width / 2, height / 2, "scenes.main-menu.background-video");
+    const video = this.add.video(
+      width / 2,
+      height / 2,
+      "scenes.main-menu.background-video"
+    );
     video.setScale(Math.max(width / VIDEO_WIDTH, height / VIDEO_HEIGHT));
     video.getFirstFrame();
     video.setAlpha(0);
 
     // Title Background
     const container = this.add.container(width / 2, height / 2);
-    const titleBackground = this.add.image(0, 0, "scenes.main-menu.title-background");
-    const buttonSinglePlayer = this.add.image(0, 0, "scenes.main-menu.button-single-player");
+    const titleBackground = this.add.image(
+      0,
+      0,
+      "scenes.main-menu.title-background"
+    );
+    const buttonSinglePlayer = this.add.image(
+      0,
+      0,
+      "scenes.main-menu.button-single-player"
+    );
     buttonSinglePlayer.setPosition(
       titleBackground.x - buttonSinglePlayer.displayWidth / 2 + 20,
-      titleBackground.y + titleBackground.displayHeight / 2 - buttonSinglePlayer.displayHeight - 50
+      titleBackground.y +
+        titleBackground.displayHeight / 2 -
+        buttonSinglePlayer.displayHeight -
+        50
     );
-    const buttonMultiplayer = this.add.image(0, 0, "scenes.main-menu.button-multiplayer");
+    const buttonMultiplayer = this.add.image(
+      0,
+      0,
+      "scenes.main-menu.button-multiplayer"
+    );
     buttonMultiplayer.setPosition(
       titleBackground.x + buttonMultiplayer.displayWidth / 2 + 20,
-      titleBackground.y + titleBackground.displayHeight / 2 - buttonMultiplayer.displayHeight - 50
+      titleBackground.y +
+        titleBackground.displayHeight / 2 -
+        buttonMultiplayer.displayHeight -
+        50
     );
-    const copyrights = this.add.image(0, titleBackground.y + titleBackground.displayHeight / 2 - 50, "scenes.main-menu.copyrights");
+    const copyrights = this.add.image(
+      0,
+      titleBackground.y + titleBackground.displayHeight / 2 - 50,
+      "scenes.main-menu.copyrights"
+    );
     container.add(titleBackground);
     container.add(buttonSinglePlayer);
     container.add(buttonMultiplayer);
@@ -36,7 +62,11 @@ export class MainMenuScene extends Phaser.Scene {
     container.setAlpha(0);
 
     // Title
-    const title = this.add.image(width / 2, height / 2, "scenes.main-menu.title");
+    const title = this.add.image(
+      width / 2,
+      height / 2,
+      "scenes.main-menu.title"
+    );
     title.setAlpha(0);
 
     // Animations
@@ -52,14 +82,14 @@ export class MainMenuScene extends Phaser.Scene {
     );
 
     video.play(true);
-    
+
     this.tweens.add({
       targets: container,
       delay: 2_000,
       props: {
         alpha: { from: 0, to: 1 },
         scale: { from: 1.5, to: 1 },
-      }
+      },
     });
 
     this.tweens.add({
@@ -68,7 +98,7 @@ export class MainMenuScene extends Phaser.Scene {
       props: {
         alpha: { from: 0, to: 1 },
         scale: { from: 0, to: 1 },
-      }
+      },
     });
   }
 }
